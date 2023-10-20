18f738b0abe9afc7c17afd0aa47889821f300950
Sam Page <samnpage96@gmail.com>
2023-10-19 20:26:48 -0400

    Added RestaurantDbContext to app services so it will be available for use in other classes

--- RestaurantRater.WebApi/Program.cs
+++ RestaurantRater.WebApi/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantRater.WebApi;
+using RestaurantRater.WebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<RestaurantDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
