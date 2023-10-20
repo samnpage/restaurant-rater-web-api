e46798328e63142a0f5a07f1a1107d26494c1d49
Sam Page <samnpage96@gmail.com>
2023-10-19 20:20:16 -0400

    DbSets added

--- RestaurantRater.WebApi/RestaurantDbContext.cs
+++ RestaurantRater.WebApi/RestaurantDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using RestaurantRater.WebApi.Data;
 
 namespace RestaurantRater.WebApi;
 
@@ -6,4 +7,8 @@ public class RestaurantDbContext : DbContext
 {
     // Constructor that inherits from the base (DbContext) controller
     public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options) { }
+
+    // DbSets to represent our tables
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
 }
